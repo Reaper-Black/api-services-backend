dd1418418755aff1f3fbc92016c24f3d9dd185fb
andreschacon <johnstartreck@gmail.com>
2022-10-02 00:07:25 -0500

    arreglo de las rutas

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express =  require('express');
 const app =  express();
+const cors =  require('cors')
 
 require('./database');
 
+app.use(cors())
 app.use(express.json());
 
 app.use('/apiservices',require('./routes/index'));
